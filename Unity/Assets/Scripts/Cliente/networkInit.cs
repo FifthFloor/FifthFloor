8a79ff4a87b7a918be67127c6f663610bdc65443
Cristhian Camilo Gomez N <camilogo1200@gmail.com>
2012-10-29 21:34:56 -0500

    Error en NetworkView

---
+++ Unity/Assets/Scripts/Cliente/networkInit.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class networkInit : MonoBehaviour {
+
+    void Awake() 
+    { /*
+        if (networkView.isMine)
+        {
+
+            MouseLook m = (MouseLook)GetComponent(typeof(MouseLook));
+            m.enabled = true;
+            CharacterController cc = (CharacterController)GetComponent(typeof(CharacterController));
+            cc.enabled = true;
+            FPSInputController FPS = (FPSInputController)GetComponent(typeof(FPSInputController));
+            FPS.enabled = true;
+            CharacterMotor cm = (CharacterMotor)GetComponent(typeof(CharacterMotor));
+            cm.enabled = true;
+
+        }
+       else
+        {
+            MouseLook m = (MouseLook)GetComponent(typeof(MouseLook));
+            m.enabled = false;
+            CharacterController cc = (CharacterController)GetComponent(typeof(CharacterController));
+            cc.enabled = false;
+            FPSInputController FPS = (FPSInputController)GetComponent(typeof(FPSInputController));
+            FPS.enabled = false;
+            CharacterMotor cm = (CharacterMotor)GetComponent(typeof(CharacterMotor));
+            cm.enabled = false;
+        }*/
+    
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Unity/Assets/Scripts/Tuts/RPCTEST.cs
+++ Unity/Assets/Scripts/Tuts/RPCTEST.cs
@@ -3,29 +3,47 @@ using System.Collections;
 
 public class RPCTEST : MonoBehaviour {
     public GameObject objeto;
+    
+    private bool flag = true;
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
-	
+
+    void OnNetworkInstantiate(NetworkMessageInfo info)
+    {
+        Debug.Log(networkView.viewID + " spawned"); 
+        if (Network.isServer) 
+        { Network.RemoveRPCs(networkView.viewID);
+            Network.Destroy(gameObject); 
+        } 
+    }
 	// Update is called once per frame
 	void Update () {
-	
 	}
-    
-    
-    
-     void makeSphere() {
 
-        Network.Instantiate(objeto, transform.position, transform.rotation,0);
+
+
+  
+    void makeSphere()
+    {
+        if (networkView.isMine)
+        {
+            Network.Instantiate(objeto, transform.position, transform.rotation, 0);
+        }
     }
 
     void OnGUI() {
-
-        if (GUILayout.Button("sphere")) 
+        
+        if (flag)
         {
-          //  networkView.RPC("makeSphere",RPCMode.Others);
-            makeSphere();
+            if (GUILayout.Button("First person Controller"))
+            {
+                //  networkView.RPC("makeSphere",RPCMode.Others);
+                makeSphere();
+                flag = !flag;
+            }
+            
         }
     }
 }
